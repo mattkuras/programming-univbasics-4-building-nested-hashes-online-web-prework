8ba931d8d24a2e7b6842a3e35eff2605068a26e1
Matt Kuras <mattkuras@gmail.com>
2020-02-14 21:39:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
 	    pieces: 4,
 	    names: {
 	      reading_railroad: {},
-	      key1: a_value,
+	      pennsylvania_railroad: {},
 	      key2: a_value,
 	      key3: a_value
 	    },
